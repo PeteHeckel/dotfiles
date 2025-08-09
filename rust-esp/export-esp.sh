a330fec488030e484b8e92fa1c346c42cf8ac708
Peter Heckel <heckel.pete@gmail.com>
2025-08-09 01:52:49 -0600

    Added rust-esp module

---
+++ rust-esp/export-esp.sh
@@ -0,0 +1,5 @@
+
+# This file is not meant to be sourced at startup in an *rc file,
+# it is intended to be sourced in rust-esp build environments as needed
+export LIBCLANG_PATH="/home/peter/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-17.0.1_20240419/esp-clang/lib"
+export PATH="/home/peter/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
